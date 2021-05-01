6a8c5fb429108a9e0cb4eb121d35f4d8062e8f6c
yuncheng <hanyuncheng@outlook.com>
2021-05-01 17:59:22 +0800

    1

--- ActionRPG/Player.gd
+++ ActionRPG/Player.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
 
+export var ACCELERATION = 500 # 加速度
+export var MAX_SPEED = 80 # 最大速度
+export var FRICTIION = 500 # 摩擦力
+
+
 var velocity = Vector2.ZERO # 速度
-export var ACCELERATION = 10 # 加速度
-export var MAX_SPEED = 100 # 最大速度
-export var FRICTIION = 10 # 摩擦力
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO # 输入速度
@@ -12,8 +14,9 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * delta * ACCELERATION
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = velocity.move_toward(velocity.ZERO, FRICTIION*delta)
-	move_and_collide(velocity)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTIION * delta)
+	
+	velocity = move_and_slide(velocity)
+	print(velocity)
